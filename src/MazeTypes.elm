5f7b7d79f455470bcd23aed3df3a8c79bf362ec3
kyhe <kyhe@uchicago.edu>
2019-05-20 20:07:03 -0500

    Basic maze render kind of working

---
+++ src/MazeTypes.elm
@@ -0,0 +1,33 @@
+module MazeTypes exposing (..)
+
+import Array exposing (Array)
+
+type alias Coordinate = (Int, Int)
+
+type Direction = N | E | W | S
+
+type Difficulty = Easy | Medium | Hard
+type Player = Human | AI | None
+type GameState = AIVictory | PlayerVictory | Started | Stopped
+
+type alias Grid a = Array (Array a)
+type alias Maze = Grid Node
+
+type alias Model =
+    {
+        playerMaze : Maze,
+        aiMaze : Maze,
+        player : Coordinate,
+        ai : Coordinate,
+        difficulty : Difficulty,
+        winner : Player,
+        gameState : GameState,
+        winningCoord : Coordinate
+    }
+
+type alias Node =
+    { n : Bool
+    , e : Bool
+    , w : Bool
+    , s : Bool
+    }
